e888fc1ba24df8ccbae1a0c8876c74e691a7712e
Aelys <AelysCh@proton.me>
2023-11-23 21:30:36 -0300

    Refactor weather command to use embed

--- src/commands/Embed_Weather.js
+++ src/commands/Embed_Weather.js
@@ -8,7 +8,7 @@ module.exports = {
     .setDescription('gives you an embed with rich information!')
     .addStringOption(option =>
         option.setName('city')
-        .setDescription('The city to get the weather from')
+        .setDescription('The city to get the weather from ')
         .setRequired(true)),
 
     async execute(interaction) {
@@ -16,11 +16,16 @@ module.exports = {
         const city = options.getString('city');
         const capitalized_city = city.charAt(0).toUpperCase() + city.slice(1);
         const apiResponse = await Get_Weather(city);
+        const region = apiResponse.location.region;
+        const weather_description = apiResponse.current.weather_descriptions;
         const weather = apiResponse.current.temperature;
         const embed = new EmbedBuilder()
         .setColor("Purple")
-        .setTitle(`Weather in ${city}`)
-        .setDescription(`${capitalized_city} is currently ${weather}°C`)
+        .setThumbnail("https://cdn-icons-png.flaticon.com/512/1163/1163657.png")
+        .setImage("https://github.com/kitsunebishi/Wallpapers/blob/main/images/00239.jpg?raw=true")
+        .setTitle(`Weather in ${capitalized_city}`)
+        .setDescription(`${capitalized_city} [${region}] is currently ${weather}°C`)
+        .addFields({ name: "Weather description", value: String(weather_description)})
         .setTimestamp()
 
     
