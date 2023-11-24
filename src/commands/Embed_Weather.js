0f6769fee88eadedb0f18445a8c87f411614957a
Aelys <AelysCh@proton.me>
2023-11-24 12:28:32 -0300

    Refactor Embed_Weather command and weather-service.js

--- src/commands/Embed_Weather.js
+++ src/commands/Embed_Weather.js
@@ -9,26 +9,37 @@ module.exports = {
     .addStringOption(option =>
         option.setName('city')
         .setDescription('The city to get the weather from ')
-        .setRequired(true)),
+        .setRequired(true))
+    .addStringOption(option => 
+        option.setName('medition')
+        .setDescription('The medition system to use')
+        .setRequired(false)),
 
     async execute(interaction) {
+        var medition_type = '°C'
         const { options } = interaction;
         const city = options.getString('city');
         const capitalized_city = city.charAt(0).toUpperCase() + city.slice(1);
-        const apiResponse = await Get_Weather(city);
+        const medition = options.getString('medition') || 'C';
+        const apiResponse = await Get_Weather(city, medition);        
+        const country = apiResponse.location.country;
         const region = apiResponse.location.region;
         const weather_description = apiResponse.current.weather_descriptions;
+        const weather_icon = apiResponse.current.weather_icons;
         const weather = apiResponse.current.temperature;
+        
+        if (medition === 'F') {
+            medition_type = '°F';
+        }
         const embed = new EmbedBuilder()
-        .setColor("Purple")
-        .setThumbnail("https://cdn-icons-png.flaticon.com/512/1163/1163657.png")
-        .setImage("https://github.com/kitsunebishi/Wallpapers/blob/main/images/00239.jpg?raw=true")
-        .setTitle(`Weather in ${capitalized_city}`)
-        .setDescription(`${capitalized_city} [${region}] is currently ${weather}°C`)
-        .addFields({ name: "Weather description", value: String(weather_description)})
-        .setTimestamp()
+            .setColor("Purple")
+            .setThumbnail(String(weather_icon))
+            .setImage("https://github.com/kitsunebishi/Wallpapers/blob/main/images/00239.jpg?raw=true")
+            .setTitle(`Weather in ${capitalized_city}`)
+            .setDescription(` ${country} - [${region}] ${capitalized_city} is currently ${weather} ${medition_type}`)
+            .addFields({ name: "Weather description", value: String(weather_description)})
+            .setTimestamp()
 
-    
         await interaction.reply({embeds: [embed]});
     }
 }
--- src/services/weather-service.js
+++ src/services/weather-service.js
@@ -2,23 +2,20 @@ require('dotenv').config();
 const axios = require('axios');
 const api_token = process.env.API_TOKEN;
 
-
-async function Get_Weather(city) {
-
-const params = {
-    access_key: api_token,
-    query: city
-}
+async function Get_Weather(city, medition_type) {
+    const params = {
+        access_key: api_token,
+        query: city,
+        units: medition_type === 'F' ? 'f' : 'm' // Use 'f' for Fahrenheit, 'm' for Celsius
+    }
 
     try {
         const response = await axios.get('http://api.weatherstack.com/current', {params});
         const apiResponse = response.data;
         return apiResponse;
-        
     } catch (error) {
         console.log(error);
     }
 }
-
 
 module.exports = { Get_Weather };
